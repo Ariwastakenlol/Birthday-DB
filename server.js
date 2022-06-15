dfc6658c82b84e0db2fd2c9a197ad9883e043634
Ariwastakenlol <mildlyawesomejournal@gmail.com>
2022-06-15 10:49:18 +0530

    init commit

---
+++ server.js
@@ -0,0 +1,21 @@
+// Load Node modules
+var express = require('express');
+const ejs = require('ejs');
+// Initialise Express
+var app = express();
+// Render static files
+app.use(express.static('public'));
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+// Port website will run on
+app.listen(6233);
+
+// *** GET Routes - display pages ***
+// Root Route
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
+
+app.get('/upload', function (req, res) {
+    res.render('pages/upload');
+});
