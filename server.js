dbd5e8261ad8498fb1da9326750b3f027a29657d
Ariwastakenlol <mildlyawesomejournal@gmail.com>
2022-08-01 22:43:30 +0530

    nedb implementation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Load Node modules
 var express = require('express');
 const ejs = require('ejs');
+var Datastore = require('nedb');
 // Initialise Express
 var app = express();
 // Render static files
@@ -10,12 +11,18 @@ app.set('view engine', 'ejs');
 // Port website will run on
 app.listen(6233);
 
+db = new Datastore({ filename: 'names.db', autoload: true });
+
 // *** GET Routes - display pages ***
 // Root Route
 app.get('/', function (req, res) {
     res.render('pages/index');
 });
 
-app.get('/upload', function (req, res) {
-    res.render('pages/upload');
+app.get('/db', function (req, res) {
+    let doc = {name:req.query.name,
+    	birthday:req.query.birthday
+    }
+    db.insert(doc)
+    console.log(doc)
 });
